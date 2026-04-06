4caf3bad8fa3deff955c43689b3d4f76f25b1c6f
fdxx <44993923+fdxx@users.noreply.github.com>
2026-04-06 22:22:55 +0800

    fix sptest.sh

--- src/sptest_inst.sh
+++ src/sptest_inst.sh
@@ -4,23 +4,19 @@
 ###    bash <(curl -fsSL https://raw.githubusercontent.com/fdxx/great-script/refs/heads/main/src/sptest_inst.sh)
 ###############################################################################
 
-## curl -o /dev/null -X POST -T - https://speed.cloudflare.com/__up < /dev/urandom
-## curl -o /dev/null https://speed.cloudflare.com/__down?bytes=1048576000
-
 cat <<'EOF' > /usr/bin/sptest
 #!/bin/bash
 
-UPCMD='curl -o /dev/null -X POST -T - https://speed.cloudflare.com/__up < /dev/urandom'
-DOWNCMD='curl -o /dev/null https://speed.cloudflare.com/__down?bytes=1048576000'
-
 if [[ "$1" == "up" ]]; then
-    echo "$UPCMD"
-    eval "$UPCMD"
+    set -x
+    curl -o /dev/null -X POST -T - https://speed.cloudflare.com/__up < /dev/urandom
+    set +x
     exit 0
 fi
 
-echo "$DOWNCMD"
-$DOWNCMD
+set -x
+curl -o /dev/null -H "referer: https://speed.cloudflare.com/" https://speed.cloudflare.com/__down?bytes=1048576000
+set +x
 EOF
 
 chmod +x /usr/bin/sptest
